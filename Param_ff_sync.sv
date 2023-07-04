c7098bfd56540e057420eb0e49fc0250d70c664c
Jaskaran Singh <117493479+jakaransingh@users.noreply.github.com>
2023-07-04 14:13:31 -0700

    Update Param_ff_sync.sv

--- Param_ff_sync.sv
+++ Param_ff_sync.sv
@@ -1,17 +1,19 @@
 //Created BY : JASKARAN SINGH
 module ff_sync
 #(
-  parameter DEPTH = 2
+  parameter DEPTH = 2      // PARAMETER TO PASS THE DEPTH VALUE OF THE SYNCHRONIZER
   )
  (
-    input wire  clk,
-    input wire  rstn,
-    input wire  valid_i,
-    output wire valid_o
+    input wire  clk,       // GENERALLY DEST CLOCK IS USED
+    input wire  rstn,      // DEST RESET(ACTIVE LOW)
+    input wire  valid_i,   // SIGNAL TO BE SYNCED
+    output wire valid_o    // SYNC SIGNAL
     );
 
+    // DECLARES THE THE NUMBER OF REGS REQUIRED FOR SYNC
     reg [DEPTH - 1:0] sync;
-
+    // PROC BLOCK:
+    // THIS IS IS USED TO SAMPLE THE SIGNAL TO SYNCED
     always_ff @ (posedge clk or negedge rstn) 
     begin
 
@@ -23,7 +25,7 @@ module ff_sync
             sync <= {sync[DEPTH - 2 : 0], valid_i};
         end
     end
-
+    // ASSSIGN STATMENT TO PASS THE SYNCED SIGNAL
     assign valid_o = sync[DEPTH - 1];
 
 
